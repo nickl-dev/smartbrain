70f069f6a2c82826b65d8c75b1e1f4dc7a73b741
Nicholas Lal <nlalkta@gmail.com>
2023-07-29 16:43:27 -0700

    -added started code for signin

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,60 +1,100 @@
-const SignIn = ({ onRouteChange }) => {
-  return (
-    <div className="flex justify-center items-center h-[calc(100%-56px)]">
-      <div className="w-full max-w-xs m-auto">
-        <form 
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" 
-          onSubmit={() => onRouteChange('home')}
-        >
+import React, { Component } from 'react';
 
-          <p className="font-oswald text-2xl mb-4">Sign In</p>
+class SignIn extends Component {
+  constructor (props) {
+    super(props);
 
-          <div className="mb-4">
-            <label 
-              className="block text-gray-700 text-sm font-bold mb-2 font-oswald text-left" 
-              htmlFor="username"
-            >
-              Username
-            </label>
+    this.state = {
+      signInEmail : '',
+      signInPassword: ''
+    }
+  }
 
-            <input 
-              className="shadow border w-full py-2 px-3 text-gray-700 focus:shadow-outline rounded-l  outline-none focus:bg-cyan-50 focus:border-blue-500" 
-              id="username" 
-              placeholder="Username" 
-              required 
-              type="text" 
-            />
-          </div>
-        
-          <div className="mb-6">
-            <label 
-              className="block text-gray-700 text-sm font-bold mb-2 font-oswald text-left" 
-              htmlFor="password"
-            >
-              Password
-            </label>
+  onSignInEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value })
+  }
 
-            <input 
-              className="shadow border w-full py-2 px-3 text-gray-700 focus:shadow-outline rounded-l  outline-none focus:bg-cyan-50 focus:border-blue-500" 
-              id="password" 
-              placeholder="******************" 
-              required 
-              type="password" 
-            />
-          </div>
+  onSignInPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value })
+  }
+  
+  onSubmitSignin = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('http://localhost:3000/signin', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword
+      })
+    })
+
+    const data = await response.json();
+
+    if (data === 'success') this.props.onRouteChange('home');
+  }
+
+  render () {
+    return (
+      <div className="flex justify-center items-center h-[calc(100%-56px)]">
+        <div className="w-full max-w-xs m-auto">
+          <form 
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" 
+            onSubmit={this.onSubmitSignin}
+          >
+  
+            <p className="font-oswald text-2xl mb-4">Sign In</p>
+  
+            <div className="mb-4">
+              <label 
+                className="block text-gray-700 text-sm font-bold mb-2 font-oswald text-left" 
+                htmlFor="email"
+              >
+                Email
+              </label>
+  
+              <input 
+                className="shadow border w-full py-2 px-3 text-gray-700 focus:shadow-outline rounded-l  outline-none focus:bg-cyan-50 focus:border-blue-500" 
+                id="email" 
+                onChange={this.onSignInEmailChange}
+                placeholder="Email" 
+                required 
+                type="email" 
+              />
+            </div>
           
-          <div className="flex items-center justify-between">
-            <button 
-              className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-oswald focus:bg-blue-600" 
-              type="submit"
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
+            <div className="mb-6">
+              <label 
+                className="block text-gray-700 text-sm font-bold mb-2 font-oswald text-left" 
+                htmlFor="password"
+              >
+                Password
+              </label>
+  
+              <input 
+                className="shadow border w-full py-2 px-3 text-gray-700 focus:shadow-outline rounded-l  outline-none focus:bg-cyan-50 focus:border-blue-500" 
+                id="password" 
+                onChange={this.onSignInPasswordChange}
+                placeholder="******************" 
+                required 
+                type="password"
+              />
+            </div>
+            
+            <div className="flex items-center justify-between">
+              <button 
+                className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-oswald focus:bg-blue-600" 
+                type="submit"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default SignIn;
